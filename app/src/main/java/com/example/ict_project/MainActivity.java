4de5f8d31131313d9aa55e0070026fb2352fb48d
shin <dongbin4013@naver.com>
2020-03-03 00:36:23 +0900

    왼쪽 상단 이미지버튼 클릭 시 다이얼로그로 비밀번호 변경가능 변경된 비밀번호는 String형 newPW에 저장

--- app/src/main/java/com/example/ict_project/MainActivity.java
+++ app/src/main/java/com/example/ict_project/MainActivity.java
@@ -1,16 +1,22 @@
 package com.example.ict_project;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
+import android.graphics.Paint;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 
 
 public class MainActivity extends AppCompatActivity {
 
 
+    //String myPW = new String()
     ImageView[] iv_pos = new ImageView[12];
 
     @Override
@@ -18,16 +24,66 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        Shuffle();
 
-        for (int i = 1; i < 13; i++) {
-            int posId = getResources().getIdentifier("pos" + i, "id", getPackageName());
-            iv_pos[i - 1] = (ImageView) findViewById(posId);
+        ImageButton btnShuffle = (ImageButton)findViewById(R.id.btn_shuffle);
 
-            int drawableId = getResources().getIdentifier("line_" + (i - 1), "drawable", getPackageName());
-            iv_pos[i - 1].setImageResource(drawableId);
-
-
-        }
-
+        btnShuffle.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Shuffle();
+            }
+        });
     }
+
+
+    public void Shuffle()
+    {
+        boolean flag[] = new boolean[12];
+        for(int i = 1; i < 13; i++)
+        {
+                while(true) {
+                    int rnd = (int)(Math.random() * 12 ); //rnd는 0~11까지 랜덤 수
+                    if (!flag[rnd]) {
+                        int posId = getResources().getIdentifier("pos" + i, "id", getPackageName()); // @+id/pos1, @+id/pos2...를 posId에 넣는다.
+                        int drawableId = getResources().getIdentifier("line_" + rnd, "drawable", getPackageName()); // drawable/line_0, drawable/line_2를 drawableId에 넣는다
+
+                        iv_pos[i - 1] = (ImageView) findViewById(posId); //id 할당
+                        iv_pos[i - 1].setImageResource(drawableId);  // 할당된 iv_pos[]에 그림 그리기
+                        flag[rnd] = !flag[rnd];
+                        break;
+                    }
+                }
+        }
+    }
+
+    public void ChangePW(View view)
+    {
+        final EditText et = new EditText(this);
+        final AlertDialog.Builder builder = new AlertDialog.Builder(this,R.style.ChangeMyPW);
+        builder.setTitle("비밀번호 바꾸기").setMessage("변경할 비밀번호를 입력하세요");
+        builder.setIcon(R.drawable.shuffle);
+
+        builder.setView(et);
+
+        builder.setPositiveButton("저장", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                String newPW = et.getText().toString();
+            }
+        });
+        builder.setNegativeButton("취소", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+            }
+        });
+        AlertDialog alertDialog = builder.create();
+        alertDialog.show();
+    }
+
+
+
 }
+
+
+
