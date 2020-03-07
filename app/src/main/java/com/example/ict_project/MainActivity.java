9c084c4cd9cb3e3000aea433aaf6427df2abfeb2
shin <dongbin4013@naver.com>
2020-03-07 19:09:24 +0900

    블루투스 켜는 기능 구현

--- app/src/main/java/com/example/ict_project/MainActivity.java
+++ app/src/main/java/com/example/ict_project/MainActivity.java
@@ -3,21 +3,33 @@ package com.example.ict_project;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.bluetooth.BluetoothAdapter;
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.graphics.Paint;
 import android.os.Bundle;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.Toast;
+
+import static android.content.Intent.*;
 
 
 public class MainActivity extends AppCompatActivity {
 
+    BluetoothAdapter mBluetoothAdapter = null;
 
     //String myPW = new String()
     ImageView[] iv_pos = new ImageView[12];
+
+
+    final static int BT_REQUEST_ENABLE = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,8 +37,10 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         Shuffle();
+        bluetoothOn();
 
         Button btnShuffle = (Button)findViewById(R.id.btn_shuffle);
+        Button btnOn = (Button)findViewById(R.id.btn_on);
 
         btnShuffle.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -34,6 +48,15 @@ public class MainActivity extends AppCompatActivity {
                 Shuffle();
             }
         });
+        btnOn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+
+
     }
 
 
@@ -79,7 +102,22 @@ public class MainActivity extends AppCompatActivity {
         AlertDialog alertDialog = builder.create();
         alertDialog.show();
     }
-
+    public void bluetoothOn() {
+        mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+        if(mBluetoothAdapter == null) {
+            Toast.makeText(getApplicationContext(), "블루투스를 지원하지 않는 기기입니다.", Toast.LENGTH_LONG).show();
+        }
+        else {
+            if (mBluetoothAdapter.isEnabled()) {
+                Toast.makeText(getApplicationContext(), "블루투스가 이미 활성화 되어 있습니다.", Toast.LENGTH_LONG).show();
+            }
+            else {
+                Toast.makeText(getApplicationContext(), "블루투스가 활성화 되어 있지 않습니다.", Toast.LENGTH_LONG).show();
+                Intent intentBluetoothEnable = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+                startActivityForResult(intentBluetoothEnable, BT_REQUEST_ENABLE);
+            }
+        }
+    }
 
 
 }
